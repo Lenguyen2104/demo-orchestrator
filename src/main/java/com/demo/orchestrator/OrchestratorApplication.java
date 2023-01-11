a74512e31044b11ba7e54fc8dd109fba342b5929
Lê Tấn Nguyên <letannguyen@MacBook-Air-cua-Le-2.local>
2023-01-11 10:25:41 +0700

    setup eureka

--- src/main/java/com/demo/orchestrator/OrchestratorApplication.java
+++ src/main/java/com/demo/orchestrator/OrchestratorApplication.java
@@ -2,8 +2,11 @@ package com.demo.orchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrchestratorApplication {
 
     public static void main(String[] args) {
